65987066b20a2721011768cac0ba925de03ab753
javrit <javrit@Julien>
2024-10-01 22:56:08 +1000

    j'ai ajouté la clef de l'API JCDecaus je vous assure promis juré

--- api_JCdecaux.py
+++ api_JCdecaux.py
@@ -1,2 +1,2 @@
 # API URL
-DYNAMIC_DATA_URL = "https://api.jcdecaux.com/vls/v1/stations?contract={Toulouse}&apiKey={api_key}"
+DYNAMIC_DATA_URL = "https://api.jcdecaux.com/vls/v1/stations?contract={Toulouse}&apiKey={c0796b53b9a70237cb401518444ba6078ddc107b}"
