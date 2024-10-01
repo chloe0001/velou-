4cf7b6ba2364357f0ebff1787b4dc6984df2c9b9
chloe0001 <chloeweydert@gmail.com>
2024-10-01 15:49:25 +0200

    Update api_JCdecaux.py

--- api_JCdecaux.py
+++ api_JCdecaux.py
@@ -1,17 +1,5 @@
-# API URL
-#DYNAMIC_DATA_URL = "https://api.jcdecaux.com/vls/v1/stations?contract='Toulouse'&apiKey='c0796b53b9a70237cb401518444ba6078ddc107b'"
-
-#def get_dynamic_data(api_key, contract_name):
-#    url = DYNAMIC_DATA_URL.format(contract_name=contract_name, api_key=api_key)
-#    response = requests.get(url)
-#    if response.status_code == 200:
-#        dynamic_data = response.json()
-#        print("Données dynamiques récupérées avec succès.")
-#        return dynamic_data
-#    else:
-#        print(f"Erreur lors de la récupération des données dynamiques: {response.status_code}")
- #       return None
-
+import requests
+import json
 
 # API URL
 #DYNAMIC_DATA_URL = "https://api.jcdecaux.com/vls/v1/stations?contract='Toulouse'&apiKey='c0796b53b9a70237cb401518444ba6078ddc107b'"
@@ -22,6 +10,11 @@ def get_dynamic_data(api_key, contract_name):
     response = requests.get(url)
     if response.status_code == 200:
         dynamic_data = response.json()
+      # Sauvegarder les données dans un fichier JSON
+        with open('data.json', 'w', encoding='utf-8') as f:
+            json.dump(dynamic_data, f, ensure_ascii=False, indent=4)
+        
+        print("Données enregistrées dans le fichier 'data.json'.")
         print("Données dynamiques récupérées avec succès.")
         return dynamic_data
     else:
