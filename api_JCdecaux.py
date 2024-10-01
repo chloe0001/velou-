a0f05baef3024f142cec75586e6e3c2cd800dce1
chloe0001 <chloeweydert@gmail.com>
2024-10-01 15:44:23 +0200

    Update api_JCdecaux.py

--- api_JCdecaux.py
+++ api_JCdecaux.py
@@ -1,5 +1,21 @@
 # API URL
-DYNAMIC_DATA_URL = "https://api.jcdecaux.com/vls/v1/stations?contract='Toulouse'&apiKey='c0796b53b9a70237cb401518444ba6078ddc107b'"
+#DYNAMIC_DATA_URL = "https://api.jcdecaux.com/vls/v1/stations?contract='Toulouse'&apiKey='c0796b53b9a70237cb401518444ba6078ddc107b'"
+
+#def get_dynamic_data(api_key, contract_name):
+#    url = DYNAMIC_DATA_URL.format(contract_name=contract_name, api_key=api_key)
+#    response = requests.get(url)
+#    if response.status_code == 200:
+#        dynamic_data = response.json()
+#        print("Données dynamiques récupérées avec succès.")
+#        return dynamic_data
+#    else:
+#        print(f"Erreur lors de la récupération des données dynamiques: {response.status_code}")
+ #       return None
+
+
+# API URL
+#DYNAMIC_DATA_URL = "https://api.jcdecaux.com/vls/v1/stations?contract='Toulouse'&apiKey='c0796b53b9a70237cb401518444ba6078ddc107b'"
+DYNAMIC_DATA_URL = "https://api.jcdecaux.com/vls/v1/stations?contract={contract_name}&apiKey={api_key}"
 
 def get_dynamic_data(api_key, contract_name):
     url = DYNAMIC_DATA_URL.format(contract_name=contract_name, api_key=api_key)
@@ -11,3 +27,6 @@ def get_dynamic_data(api_key, contract_name):
     else:
         print(f"Erreur lors de la récupération des données dynamiques: {response.status_code}")
         return None
+
+
+get_dynamic_data('c0796b53b9a70237cb401518444ba6078ddc107b', 'Toulouse')
